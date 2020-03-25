118a862d7e28d4e15ab3ed8f07fab499aa50624b
hassan.adel <hasanadel555@hotmail.com>
2020-03-25 22:42:13 +0200

    add class

--- src/com/tutorial/UIWidget.java
+++ src/com/tutorial/UIWidget.java
@@ -3,5 +3,6 @@ package com.tutorial;
 public interface UIWidget {
     void drag();
     void resize();
+    void resize(int x, int y);
     void render();
 }
---
+++ src/com/tutorial/dragger.java
@@ -0,0 +1,8 @@
+package com.tutorial;
+
+public class dragger {
+    public void drag(UIWidget widget){
+        widget.drag();
+        System.out.println("dragging");
+    }
+}
